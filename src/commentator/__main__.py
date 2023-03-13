f725bd4b39d5a808408c9289a5f3f3c56d710d44
Emery Berger <emery.berger@gmail.com>
2023-03-13 18:37:31 -0400

    Delay progress bar display until number of functions are calculated.

--- src/commentator/__main__.py
+++ src/commentator/__main__.py
@@ -11,22 +11,24 @@ from . import commentator
 
 async def do_one_file(index, file, language):
     code = file.read()
+    try:
+        ast.parse(code)
+    except SyntaxError:
+        # Failed to parse.
+        return
+        
     function_count = 0
     for func_name in commentator.enumerate_functions(code):
         the_code = commentator.extract_function_source(code, func_name)
         if not (commentator.has_docstring(the_code) and commentator.has_types(the_code)):
             function_count += 1
             
-    from tqdm import tqdm
-    pbar = tqdm(total=1, desc=file.name, leave=False, unit='function') # position=index, 
-    
     if function_count == 0:
-        pbar.update(1)
-        # pbar.set_description(f'{file.name}: no functions need commentating.')
-        # pbar.update(1)
         return
 
-    pbar.total=function_count
+    from tqdm import tqdm
+    pbar = tqdm(total=function_count, desc=file.name, leave=False, unit='function') # position=index,
+    
     (result, successes) = await commentator.commentate(file.name, code, pbar, language)
     if result:
         save_path = "backup"
